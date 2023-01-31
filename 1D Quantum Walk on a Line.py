52b0f9a03dc31cf6038b122828ea24adfd9dfd10
larajaniurek <118266807+larajaniurek@users.noreply.github.com>
2023-01-31 10:43:54 +0000

    Update 1D Quantum Walk on a Line.py

--- 1D Quantum Walk on a Line.py
+++ 1D Quantum Walk on a Line.py
@@ -1,39 +1,19 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[15]:
-
-
 ####  1D quantum walk on a line ####
 
-
-# In[16]:
 
 
 from numpy import *                    # importing required Python packages
 from matplotlib.pyplot import * 
 
 
-# In[ ]:
 
-
-
-
-
-# In[17]:
 
 
 N = 100     # number of randoms steps 
 P = 2*N+1    # Number of possible positions on line 
 
 
-# In[ ]:
 
-
-
-
-
-# In[18]:
 
 
 #  defining coin states 
@@ -41,7 +21,7 @@ coin0 = array([1, 0])  # |0>
 coin1 = array([0, 1])  # |1>
 
 
-# In[19]:
+
 
 
 # gives outer product  of coin states 
@@ -51,7 +31,6 @@ C10 = outer(coin1, coin0)  # |1><0|
 C11 = outer(coin1, coin1)  # |1><1| 
 
 
-# In[20]:
 
 
 # combine to give coin operator that is used to flip 
@@ -59,13 +38,7 @@ C11 = outer(coin1, coin1)  # |1><1|
 C_hat = (C00 + C01 + C10 - C11)/sqrt(2.)
 
 
-# In[ ]:
 
-
-
-
-
-# In[21]:
 
 
 # shift operator moves along left or right depending
@@ -76,13 +49,7 @@ ShiftMinus = roll(eye(P), -1, axis=0)  # position left or right
 S_hat = kron(ShiftPlus, C00) + kron(ShiftMinus, C11) # kron gives tensor product 
 
 
-# In[ ]:
 
-
-
-
-
-# In[22]:
 
 
 # this is walk operator 
@@ -90,13 +57,6 @@ S_hat = kron(ShiftPlus, C00) + kron(ShiftMinus, C11) # kron gives tensor product
 U = S_hat.dot(kron(eye(P), C_hat)) 
 
 
-# In[ ]:
-
-
-
-
-
-# In[23]:
 
 
 # now we create initial state of system
@@ -109,7 +69,7 @@ posn0[N] = 1          # creating initial position matrix
 psi0 = kron(posn0, (coin0+coin1*1j)/sqrt(2))  # initial wavefunction
 
 
-# In[24]:
+
 
 
 # we take N steps by applyng the walk operator N times to the initial 
@@ -117,13 +77,7 @@ psi0 = kron(posn0, (coin0+coin1*1j)/sqrt(2))  # initial wavefunction
 psiN = linalg.matrix_power(U, N).dot(psi0)   
 
 
-# In[ ]:
 
-
-
-
-
-# In[25]:
 
 
 # now we need to measure the system!
@@ -136,13 +90,6 @@ for k in range(P):
     prob[k] = proj.dot(proj.conjugate()).real
 
 
-# In[ ]:
-
-
-
-
-
-# In[60]:
 
 
 # here we just plot the probabilities 
@@ -164,16 +111,4 @@ xlabel('Position')
 ylabel('Probability')
 
 show()             
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
 
